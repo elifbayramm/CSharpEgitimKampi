d26e713d36e6c8dfc94ff4c2598feae0aac1348c
elifbayramm <eliiifbayrammm@gmail.com>
2024-10-21 21:17:44 +0300

    7 ve 8. dersin sonu

--- 06_Arrays/Program.cs
+++ 06_Arrays/Program.cs
@@ -143,30 +143,30 @@ namespace _06_Arrays
             //}
             //Console.WriteLine(sum);
 
-            int[] numbers = { 21, 42, 33, 54, 55, 66, 897, 748, 39, 220 };
+            //int[] numbers = { 21, 42, 33, 54, 55, 66, 897, 748, 39, 220 };
 
-            Console.WriteLine("Çift Sayılar");
-            Console.WriteLine();
+            //Console.WriteLine("Çift Sayılar");
+            //Console.WriteLine();
 
-            for (int i = 0; i < numbers.Length; i++)
-            {
-                if (numbers[i] % 2 == 0)
-                {
-                    Console.WriteLine(numbers[i]);
-                }
-            }
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+            //    if (numbers[i] % 2 == 0)
+            //    {
+            //        Console.WriteLine(numbers[i]);
+            //    }
+            //}
 
-            Console.WriteLine("-------------");
-            Console.WriteLine("Tek Sayılar");
-            Console.WriteLine();
+            //Console.WriteLine("-------------");
+            //Console.WriteLine("Tek Sayılar");
+            //Console.WriteLine();
 
-            for (int i = 0; i < numbers.Length; i++)
-            {
-                if (numbers[i] % 2 == 1)
-                {
-                    Console.WriteLine(numbers[i]);
-                }
-            }
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+            //    if (numbers[i] % 2 == 1)
+            //    {
+            //        Console.WriteLine(numbers[i]);
+            //    }
+            //}
 
             #endregion
 
